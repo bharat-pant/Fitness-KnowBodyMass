629b862982015ff706d64a0d86f473e6df24d331
Bharat <bpant268@gmail.com>
2022-08-12 22:24:42 +0530

    Test application first update

--- app/src/main/java/com/example/fitness_knowbodymass/MainActivity.kt
+++ app/src/main/java/com/example/fitness_knowbodymass/MainActivity.kt
@@ -3,20 +3,36 @@ package com.example.fitness_knowbodymass
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    val height = findViewById<EditText>(R.id.editTextTextPersonName2)
-    val weight = findViewById<EditText>(R.id.editTextTextPersonName)
-    val aboutActivityButton = findViewById<Button>(R.id.button)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val height:EditText = findViewById<EditText>(R.id.input_height_edittext)
+        val weight:EditText = findViewById<EditText>(R.id.input_weight_edittext)
+        val aboutActivityButton:Button = findViewById<Button>(R.id.about_activity_button)
+        val convertButton = findViewById<Button>(R.id.convert_button)
+        val outputAnswerTextView = findViewById<TextView>(R.id.output_answer)
+
+        val inputHeight = height.text
+        val inputWeight = weight.text
+
         aboutActivityButton.setOnClickListener {
             val intent = Intent(this,AboutAppActivity::class.java)
             startActivity(intent)
+        }
+
+        convertButton.setOnClickListener{
+            Log.i("answer","" + inputHeight)
+            Log.i("answer","" + inputWeight)
+            val bmi = inputWeight.toString().toInt()/(inputHeight.toString().toFloat()*inputHeight.toString().toFloat())
+            Log.i("answer","" + bmi)
+            outputAnswerTextView.text = "%.2f".format(bmi)
         }
     }
 }
